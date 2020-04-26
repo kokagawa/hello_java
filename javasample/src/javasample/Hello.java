dd9bbf8448c9be2a4e26efb789f71115b0bd3dbb
kagawa <tekken77marduk>
2020-04-26 18:52:42 +0900

    ddd

--- javasample/src/javasample/Hello.java
+++ javasample/src/javasample/Hello.java
@@ -3,8 +3,10 @@ package javasample;
 class Hello {
 	public static void main(String[] args) {
 
-		String name = "kinghhy";
 
+		String name = "kin";
+		System.out.println("Hello " + name + "！！");
+		System.out.println("Hello " + name + "！！");
         System.out.println("Hello " + name + "！！");
         System.out.println("Hello " + name + " again！！");
     }
