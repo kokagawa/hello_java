d18867028cd46b78c1dd8e08aad0d61d372a9395
kagawa <tekken77marduk>
2020-04-26 18:27:24 +0900

    yyyyy

--- javasample/src/javasample/Hello.java
+++ javasample/src/javasample/Hello.java
@@ -3,7 +3,7 @@ package javasample;
 class Hello {
 	public static void main(String[] args) {
 
-		String name = "kin";
+		String name = "kinghhh";
 
         System.out.println("Hello " + name + "！！");
         System.out.println("Hello " + name + " again！！");
