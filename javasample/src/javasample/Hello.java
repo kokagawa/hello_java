f34021c47184ef2d517be279bd82eb7f2425b362
kagawa <tekken77marduk>
2020-04-26 19:43:00 +0900

    ddd

--- javasample/src/javasample/Hello.java
+++ javasample/src/javasample/Hello.java
@@ -4,7 +4,7 @@ class Hello {
 	public static void main(String[] args) {
 
 
-		String name = "taro";
+		String name = "note";
 		System.out.println("Hello " + name + "！！");
 		System.out.println("Hello " + name + "！！");
         System.out.println("Hello " + name + "！！");
