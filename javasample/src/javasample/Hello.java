0c4f0ee0e02d521dc5c682bb361cfa192b1c7c89
kagawa <tekken77marduk>
2020-04-26 18:34:45 +0900

    wwww

--- javasample/src/javasample/Hello.java
+++ javasample/src/javasample/Hello.java
@@ -4,7 +4,7 @@ class Hello {
 	public static void main(String[] args) {
 
 		String name = "kin";
-
+		System.out.println("Hello " + name + "！！");
         System.out.println("Hello " + name + "！！");
         System.out.println("Hello " + name + " again！！");
     }
