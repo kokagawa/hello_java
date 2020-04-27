2fb9a80ece60fedee18926e912df526dc34ed208
kagawa <tekken77marduk>
2020-04-27 14:22:49 +0900

    aaaa

--- javasample/src/javasample/Hello.java
+++ javasample/src/javasample/Hello.java
@@ -6,6 +6,7 @@ class Hello {
 
 
 		String name = "note";
+		System.out.println("Hello " + name + "！！");
         System.out.println("Hello " + name + "！！");
         System.out.println("Hello " + name + " again！！");
     }
